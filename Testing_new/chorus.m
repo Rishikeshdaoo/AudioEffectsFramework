8b74fabff0b438451714cc50d92c6e53268553b0
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-02-09 16:31:03 -0500

    Add files via upload

---
+++ Testing_new/chorus.m
@@ -0,0 +1,47 @@
+function [modulated_output, MSE] = chorus(audio_test, input, Fs, max_time_delay, modulation_depth, modulation_rate, feedback)
+% function [y, MSE] = chorus(audio_test, x, Fs, max_time_delay, rate)
+
+
+%This is a chorus effect. 
+%The structure is:  function [modulated_output, MSE] = chorus(audio_test, input, Fs, max_time_delay, modulation_depth, modulation_rate, feedback)
+%
+%where audio_test = filtered audio from C++ code (size = (2 x num_samples))
+%      input = the input signal 
+%      Fs = Sampling rate 
+%      max_time_delay = delay time in seconds
+%      modulation_depth = depth of modulation in seconds
+%      modulation_rate = frequency of LFO in Hz
+%      feedback = feedback amount (0.0 for all dry, 1.0 for all wet)
+%
+% Source: https://github.com/danpprince/matlab-chorus/
+
+
+delay_length_samples     = round(max_time_delay * Fs);
+modulation_depth_samples = round(modulation_depth * Fs);
+
+modulated_output = zeros(length(input), 1);
+delay_buffer     = zeros(delay_length_samples + modulation_depth_samples, 1);
+
+% Argument for sin() modulation function. Converts the loop's control variable into 
+% the appropriate argument in radians to achieve the specified modulation rate
+modulation_argument = 2 * pi * modulation_rate / Fs;
+
+
+for i = 1:(length(input))
+	% Find index to read from for modulated output
+	modulated_sample = modulation_depth_samples * sin(modulation_argument * i);
+	modulated_sample = modulated_sample + delay_length_samples;
+
+	% Get values to interpolate between
+	interp_y1 = delay_buffer(floor(modulated_sample));
+	interp_y2 = delay_buffer( ceil(modulated_sample));
+
+	query_sample = modulated_sample - floor(modulated_sample);
+
+	% Interpolate to find the output value
+	modulated_output(i) = interp_y1 + (interp_y2 - interp_y1) * (query_sample);
+
+	% Save the input's current value in the buffer and advance to the next value
+	new_sample = (input(i) + modulated_output(i) * feedback);
+	delay_buffer = [ new_sample; delay_buffer(1 : length(delay_buffer)-1) ];
+end
---
+++ Testing_new/schroeder2.m
@@ -0,0 +1,30 @@
+function [y,b,a]=schroeder(x,n,g,d,k)
+%This is a reverberator based on Schroeder?s design which consists of n all
+%pass filters in series.
+%
+%The structure is: [y,b,a] = schroeder1(x,n,g,d,k)
+%
+%where x = the input signal
+%      n = the number of allpass filters
+%      g = the gain of the allpass filters (should be less than 1 for stability)
+%      d = a vector which contains the delay length of each allpass filter
+%      k = the gain factor of the direct signal
+%      y = the output signal
+%      b = the numerator coefficients of the transfer function
+%      a = the denominator coefficients of the transfer function
+%
+% Chapter 7: Digital Audio Effects by Prof David Marshall and Dr Kirill Sidorov, 
+% Cardiff School of Computer Science.
+
+
+[y,b,a] = allpass(x,g,d(1));
+% send the output of each allpass filter to the input of the next allpass filter
+for i = 2:n,
+[y,b1,a1] = allpass(y,g,d(i));
+[b,a] = seriescoefficients(b1,a1,b,a);
+end
+
+% add the scaled direct signal
+y = y + k*x;
+% normalize the output signal
+y = y/max(y);
