3c4a0027d438172367925208db3e5f321086f421
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-02-12 14:42:21 -0500

    Update main.m

--- Testing/main.m
+++ Testing/main.m
@@ -12,4 +12,12 @@ audio_path_cpp = 'Guitar_sample.wav'; % same as raw now
 param_flanger = [Fs, 0.002, 1]; % params for flanger effect: [Fs, max_time_delay, rate]
 [audio_mat_flanger, MSE_flanger] = test(audio_raw, audio_cpp, "flanger", param_flanger);
 
+param_chorus = [Fs, 0.013, 0.003, 1, 0.8] % params for chorus effect: [Fs, max_time_delay, modulation_depth, modulation_rate, feedback]
+[audio_mat_chorus, MSE_chorus] = test(audio_raw, audio_cpp, "chorus", param_chorus);
+
+rand(’state’,sum(100*clock))
+param_reverb = [6, 0.9, floor(0.05*rand([1,n])*Fs), 0.2] % params for reverb effect: [no. of all pass filters,list of gains,list of delay times, gain]
+[audio_mat_reverb, MSE_reverb] = test(audio_raw, audio_cpp, "chorus", param_reverb);
+
+
 %% other test
