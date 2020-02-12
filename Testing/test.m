53e1675ede91912bdbb09b54486bfee31470c771
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-02-12 14:30:04 -0500

    Update test.m

--- Testing/test.m
+++ Testing/test.m
@@ -17,6 +17,11 @@ switch effect
     case "flanger"
         audio_mat = flanger(audio_raw, param(1), param(2), param(3));
         MSE = compare(audio_cpp, audio_mat);
-        
+    case "chorus"
+        audio_mat = flanger(audio_raw, param(4), param(5), param(6), param(7), param(8));
+        MSE = compare(audio_cpp, audio_mat);
+    case "reverb"
+        audio_mat = flanger(audio_raw, param(9), param(10), param(11), param(12));
+        MSE = compare(audio_cpp, audio_mat);
 end
 end
