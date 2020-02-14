978692b3fbcaa70436515a19fc21eff0be0cf52d
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-02-14 10:42:54 -0500

    Added EffectDef.h

---
+++ inc/EffectDef.h
@@ -0,0 +1,24 @@
+//
+// Created by Rishikesh Daoo on 2/13/20.
+//
+
+#ifndef AUDIOFXFRAMEWORK_EFFECTTYPE_H
+#define AUDIOFXFRAMEWORK_EFFECTTYPE_H
+
+#endif //AUDIOFXFRAMEWORK_EFFECTTYPE_H
+
+
+enum Effect_t
+{
+    kGain,
+    kPan,
+    kDistortion,
+    kCompressor,
+    kDelay,
+    kReverb,
+    kBiquad,
+    kFlanger,
+    kPanorama,
+    kChorus,
+    kPitchShift,
+};
