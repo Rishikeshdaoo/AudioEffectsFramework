d4aa55b993f4798535faddb1430ab2100645d95a
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-04-08 13:14:39 -0400

    Update flanger.m

--- Testing/effects/flanger.m
+++ Testing/effects/flanger.m
@@ -1,28 +1,28 @@
-function [y] = flanger(x, Fs, max_time_delay, rate)
+function [y] = flanger(x, Fs, MaxDelayinSecs, ModRateinHz)
 
 %This is a flanger effect. Creates a single FIR delay with the delay time oscillating from
 % Either 0-3 ms or 0-15 ms at 0.1 - 5 Hz
 %
-%The structure is:  [y] = flanger(audio_tset, x,Fs, max_time_delay, rate)
+%The structure is:  [y] = flanger(audio_tset, x,Fs, MaxDelayinSecs, ModRateinHz)
 %
 %where audio_test = filtered audio from C++ code 
 %      x = the input signal 
 %      Fs = Sampling rate 
-%      max_time_delay = delay time in seconds
-%      rate = rate of flange in Hz
+%      MaxDelayinSecs = delay time in seconds
+%      ModRateinHz = rate of flange in Hz
 %
 % Chapter 7: Digital Audio Effects by Prof David Marshall and Dr Kirill Sidorov, 
 % Cardiff School of Computer Science.
 
 index=1:length(x);
 % sin reference to create oscillating delay
-sin_ref = (sin(2*pi*index*(rate/Fs)))';
+sin_ref = (sin(2*pi*index*(ModRateinHz/Fs)))';
 %convert delay in ms to max delay in samples
-max_samp_delay=round(max_time_delay*Fs);
+MaxDelayinSamps=round(MaxDelayinSecs*Fs);
 % create empty output vector
 y = zeros(length(x),1);
 % to avoid referencing of negative samples
-y(1:max_samp_delay)=x(1:max_samp_delay);
+y(1:MaxDelayinSamps)=x(1:MaxDelayinSamps);
 % set amp suggested coefficient from page 71 DAFX
 amp=0.7;
 
