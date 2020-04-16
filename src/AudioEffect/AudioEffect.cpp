a98659f4e93e419710770933aab87ea475f02565
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-04-16 15:57:44 -0400

    Removed commented out code

--- src/AudioEffect/AudioEffect.cpp
+++ src/AudioEffect/AudioEffect.cpp
@@ -26,30 +26,3 @@ Effect_t CAudioEffect::getEffectType()
 {
     return m_eEffectType;
 }
-
-//Error_t CAudioEffect::init(Effect_t eEffectType, EffectParam_t params[], int iNumParams, float fSampleRateInHz, int iNumChannels)
-//{
-//    std::cout << "Init Intern from Base Class." << std::endl;
-//    return kNoError;
-//};
-//
-//Error_t CAudioEffect::reset()
-//{
-//    return kNoError;
-//};
-//
-//Error_t CAudioEffect::setParam(EffectParam_t eParam, float fValue)
-//{
-//    return kNoError;
-//};
-//
-//float CAudioEffect::getParam(EffectParam_t eParam)
-//{
-//    return kNoError;
-//};
-//
-//Error_t CAudioEffect::process(float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames)
-//{
-//    std::cout << "Process Base Class" << std::endl;
-//    return kNoError;
-//};
